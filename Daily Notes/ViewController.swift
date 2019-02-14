f558a85a181edc51d586b12d8c288d54ebbf3876
Maurício Oliveira <e810@itsector.pt>
2019-02-14 15:17:47 +0000

    Initial Commit

---
+++ Daily Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Notes
+//
+//  Created by Maurício Oliveira on 14/02/2019.
+//  Copyright © 2019 Maurício Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
